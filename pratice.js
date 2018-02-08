3215867fe2cc585c57775c4271954e746a81fb12
unknown <>
2018-02-08 23:50:52 +0800

    simple

--- pratice.js
+++ pratice.js
@@ -30,52 +30,73 @@ var density = {
     '臺東縣':738.47,
     '花蓮縣':403.55
 };
-
-var svg = d3.select('#map')
+var datapath = "./data1.csv"
+d3.csv(datapath,function(dataset){
+    var svg = d3.select('#map')
     .append('svg')
     .attr('height', 600)
     .attr('width', 500)
     .attr('transform', 'translate(0,0)')
     
- var color = d3.scale.linear().domain([200,1600]).range(["#090","#f00"]);
-//var fisheye = d3.fisheye.circular().radius(100).distortion(2);  
-/*var zoom = d3.behavior.zoom()
-    .scaleExtent([1, 10])
-    .on("zoom", zoomed);
+    var color = d3.scale.linear().domain([200,1600]).range(["#FFFFFF","#003377"]);
+    //var fisheye = d3.fisheye.circular().radius(100).distortion(2);  
+    /*var zoom = d3.behavior.zoom()
+        .scaleExtent([1, 10])
+        .on("zoom", zoomed);
 
-function zoomed() {
-    map.attr("transform",
-        "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-}*/
+    function zoomed() {
+        map.attr("transform",
+            "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    }*/
 
-var features = topojson.feature(topodata, topodata.objects.COUNTY_MOI_1060525).features;
+    var features = topojson.feature(topodata, topodata.objects.COUNTY_MOI_1060525).features;
 
-var projection = d3.geo.mercator().center([121, 24]).scale(8000);
-var path = d3.geoPath().projection(projection);
-console.log(features);
+    var projection = d3.geo.mercator().center([121, 24]).scale(8000);
+    var path = d3.geoPath().projection(projection);
+    var temp = 0;
 
-for(var i=features.length - 1; i >= 0; i-- ) {
-    features[i].properties.density = density[features[i].properties.COUNTYNAME];
-  }
-console.log(features[0].properties.density);
-var map = svg.append('g')
-    .attr('transform', 'translate(-180,0)')
-    //.call(zoom)
-    .selectAll('.county')
-    .data(features)
-    .enter()
-    .append("path")
-    .attr('class', 'county')
-    .attr('d', path)
-    .attr('fill', function (d, i) {
-        return color(d.properties.density);
-    })
-    //.style('opacity', '0.5')
-    .on("mouseover", function (d) {
-        //d3.select(this).style('opacity', '1.0');
-        $("#countyname").text(d.properties.COUNTYNAME+" 每一位醫師需要服務"+d.properties.density+"人");
-    })/*
-    .on("mouseout", function (d) {
-        d3.select(this).style('opacity', '0.5');
-    })*/
-    //
+    for(var i=features.length - 1; i >= 0; i-- ) {
+        for(var j=features.length - 1; j >= 0; j-- ){
+            if(dataset[j].county == features[i].properties.COUNTYNAME){     
+                temp++;
+                features[i].properties.density = dataset[j].serve;
+                features[i].properties.doctor = dataset[j].doctor;
+                features[i].properties.population = dataset[j].population;
+            }
+        }
+    }
+    /*for(var i=features.length - 1; i >= 0; i-- ) {
+         console.log(features[i].properties.COUNTYNAME+" "+features[i].properties.doctor+" "+features[i].properties.population+" "+features[i].properties.density);
+    }*/
+    //console.log(dataset);
+
+
+    var map = svg.append('g')
+        .attr('transform', 'translate(-180,0)')
+        //.call(zoom)
+        .selectAll('.county')
+        .data(features)
+        .enter()
+        .append("path")
+        .attr('class', 'county')
+        .attr('d', path)
+        .attr('fill', function (d, i) {
+            return color(d.properties.density);
+        })
+        //.style('opacity', '0.5')
+        .on("mouseover", function (d) {
+            d3.select(this).style('stroke-width', '2');
+            d3.select(this).style('stroke', '#FB7F7F');
+            $("#countyname").text(d.properties.COUNTYNAME);
+            $("#population").text("       人口數量 : "+d.properties.population);
+            $("#doctor")    .text("       醫師數量 : "+d.properties.doctor);
+            $("#serve")     .text("每位醫生服務人數 : "+d.properties.density);
+        })
+        .on("mouseout", function (d) {
+            d3.select(this).style('stroke-width', '0');
+            d3.select(this).style('stroke', '#C6C6C6');
+            
+        })
+        //
+
+});
