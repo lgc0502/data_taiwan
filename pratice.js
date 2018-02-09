48da3fe6a4d2012853d79830407ed9d7d4a2ebc9
unknown <>
2018-02-09 23:09:58 +0800

    1/3

--- pratice.js
+++ pratice.js
@@ -6,30 +6,7 @@ var margin = {
     },
     height = 400 - margin.top - margin.bottom,
     width = 800 - margin.left - margin.right;
-var density = {
-    '連江縣':787.19,
-    '金門縣':1589.58,
-    '宜蘭縣':690.10,
-    '彰化縣':638.78,
-    '南投縣':798.05,
-    '雲林縣':788.73,
-    '屏東縣':735.09,
-    '澎湖縣':732.36,
-    '基隆市':518.25,
-    '新竹市':521.88,
-    '臺北市':227.36,
-    '新北市':772.51,
-    '臺中市':467.76,
-    '臺南市':538.25,
-    '桃園市':561.65,
-    '苗栗縣':1005.74,
-    '新竹縣':1131.16,
-    '嘉義市':309.84,
-    '嘉義縣':644.15,
-    '高雄市':452.52,
-    '臺東縣':738.47,
-    '花蓮縣':403.55
-};
+
 var datapath = "./data1.csv"
 d3.csv(datapath,function(dataset){
     var svg = d3.select('#map')
@@ -62,6 +39,7 @@ d3.csv(datapath,function(dataset){
                 features[i].properties.density = dataset[j].serve;
                 features[i].properties.doctor = dataset[j].doctor;
                 features[i].properties.population = dataset[j].population;
+                features[i].properties.rank = dataset[j].rank;
             }
         }
     }
@@ -88,9 +66,10 @@ d3.csv(datapath,function(dataset){
             d3.select(this).style('stroke-width', '2');
             d3.select(this).style('stroke', '#FB7F7F');
             $("#countyname").text(d.properties.COUNTYNAME);
-            $("#population").text("       人口數量 : "+d.properties.population);
-            $("#doctor")    .text("       醫師數量 : "+d.properties.doctor);
-            $("#serve")     .text("每位醫生服務人數 : "+d.properties.density);
+            $("#population").text("人口數量 : "+d.properties.population+"人");
+            $("#doctor")    .text("醫師數量 : "+d.properties.doctor+"人");
+            $("#servenum")     .text(d.properties.density+"人");
+            $("#ranknum").text(d.properties.rank+"/22");
         })
         .on("mouseout", function (d) {
             d3.select(this).style('stroke-width', '0');
