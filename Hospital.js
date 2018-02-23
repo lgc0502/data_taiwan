fabd5fe6d79c2fa1a14d32d0cf5a798b8ecf1423
unknown <>
2018-02-23 22:20:05 +0800

    3/4 finish

--- Hospital.js
+++ Hospital.js
@@ -2,8 +2,10 @@ var svg = d3.select('.bar')
 .append('svg')
 .attr('height', 600)
 .attr('width', 900)
-
-
+var rect = svg.append('g')
+            .attr('id','rect')
+var town = svg.append('g')
+            .attr('id','town')
 datapath="./hospital.csv"
 
 d3.csv(datapath,function(dataset){
@@ -18,7 +20,7 @@ d3.csv(datapath,function(dataset){
         .range([5, 450])
         .domain([minBar, maxBar]); //x 的最大值與最小值
 
-    svg.selectAll('rect')
+    rect.selectAll('rect')
     .data(dataset)
     .enter()
     .append('rect')
@@ -40,7 +42,21 @@ d3.csv(datapath,function(dataset){
         return scaleBar(d.center);
     })
 
-    svg.selectAll('text')
+    town.selectAll('text')
+    .data(dataset)
+    .enter()
+    .append('text')
+    .text(function(d){ return d.county})
+    .attr('x',function(d,i){
+        return i*40+13;
+    })
+    .attr('y',function(d,i){
+        return 500+10;
+    })
+    .attr('writing-mode', 'vertical-lr')
+    .attr('font-size','20px')
+
+    rect.selectAll('text')
     .data(dataset)
     .enter()
     .append('text')
@@ -56,11 +72,14 @@ d3.csv(datapath,function(dataset){
     .attr('y',function(d,i){
         return 500-scaleBar(d.center)-10;
     })
-    .tween("text", function(d) {
+    .tween("text", function(d,i) {
         var that = d3.select(this),
-            i = d3.interpolateRound(0, +d.center);
-        return function(t) { that.text(i(t)); };
+            k = d3.interpolateRound(0, +d.center);
+            data[i]=+d.center;
+        return function(t) { that.text(k(t)); };
       })
+    
+    console.log(data);
     d3.select('.btn-outline-success').on('click',function(){
         minBar = d3.min(dataset,function(d){ return +d.local});
         maxBar = d3.max(dataset,function(d){ return +d.local})
@@ -68,7 +87,7 @@ d3.csv(datapath,function(dataset){
         .range([20, 450])
         .domain([minBar, maxBar]); 
 
-        svg.selectAll('rect')
+        rect.selectAll('rect')
         .data(dataset)
         .transition()
         .duration(500)
@@ -91,7 +110,7 @@ d3.csv(datapath,function(dataset){
         })
         .attr('fill','#81C853')
 
-        svg.selectAll('text')
+        rect.selectAll('text')
         .data(dataset)
         .text(function(d){ return d.local})
         .transition()
@@ -101,15 +120,21 @@ d3.csv(datapath,function(dataset){
             return i*40+10;
         })
         .text('0')
+        .tween("text", function(d,i) {
+            var that = d3.select(this),
+                k = d3.interpolateRound(data[i], 0);
+            return function(t) { that.text(k(t)); };
+          })
         .transition()
         .duration(1500)
         .attr('y',function(d,i){
             return 500-scaleBar(d.local)-10;
         })
-        .tween("text", function(d) {
+        .tween("text", function(d,i) {
             var that = d3.select(this),
-                i = d3.interpolateRound(0, +d.local);
-            return function(t) { that.text(i(t)); };
+                k = d3.interpolateRound(0, +d.local);
+                data[i]=+d.local;
+            return function(t) { that.text(k(t)); };
           })
     });
 
@@ -120,7 +145,7 @@ d3.csv(datapath,function(dataset){
         .range([5, 450])
         .domain([minBar, maxBar]); 
 
-        svg.selectAll('rect')
+        rect.selectAll('rect')
         .data(dataset)
         .transition()
         .duration(500)
@@ -143,7 +168,7 @@ d3.csv(datapath,function(dataset){
         })
         .attr('fill','#EEC328')
 
-        svg.selectAll('text')
+        rect.selectAll('text')
         .data(dataset)
         .text(function(d){ return d.region})
         .transition()
@@ -153,15 +178,195 @@ d3.csv(datapath,function(dataset){
             return i*40+10;
         })
         .text('0')
+        .tween("text", function(d,i) {
+            var that = d3.select(this),
+                k = d3.interpolateRound(data[i], 0);
+            return function(t) { that.text(k(t)); };
+          })
         .transition()
         .duration(1500)
         .attr('y',function(d,i){
             return 500-scaleBar(d.region)-10;
         })
-        .tween("text", function(d) {
+        .tween("text", function(d,i) {
             var that = d3.select(this),
-                i = d3.interpolateRound(0, +d.region);
-            return function(t) { that.text(i(t)); };
+                k = d3.interpolateRound(0, +d.region);
+                data[i]=+d.region;
+            return function(t) { that.text(k(t)); };
+          })
+    });
+
+    d3.select('.block3').select('.btn-outline-info').on('click',function(){
+        minBar = d3.min(dataset,function(d){ return +d.hos});
+        maxBar = d3.max(dataset,function(d){ return +d.hos})
+        scaleBar = d3.scale.linear()
+        .range([5, 450])
+        .domain([minBar, maxBar]); 
+        
+        rect.selectAll('rect')
+        .data(dataset)
+        .transition()
+        .duration(500)
+        .attr('y',function(d,i){
+            return 500;
+        })
+        .attr('x',function(d,i){
+            return i*40;
+        })
+        .attr('width',30)
+        .attr('height',5)
+        .transition()
+        .duration(1500)
+        .attr('y',function(d,i){
+            return 500-scaleBar(d.hos);
+        })
+        .attr('height',function(d){
+            return scaleBar(d.hos);
+
+        })
+        .attr('fill','#009D91')
+
+        rect.selectAll('text')
+        .data(dataset)
+        .text(function(d){ return d.hos})
+        .transition()
+        .duration(500)
+        .attr('y',500-10)
+        .attr('x',function(d,i){
+            return i*40+10;
+        })
+        .text('0')
+        .tween("text", function(d,i) {
+            var that = d3.select(this),
+                k = d3.interpolateRound(data[i], 0);
+            return function(t) { that.text(k(t)); };
+          })
+        .transition()
+        .duration(1500)
+        .attr('y',function(d,i){
+            return 500-scaleBar(d.hos)-10;
+        })
+        .tween("text", function(d,i) {
+            var that = d3.select(this),
+                k = d3.interpolateRound(0, +d.hos);
+                data[i]=+d.hos;
+            return function(t) { that.text(k(t)); };
+          })
+    });
+
+    d3.select('.block3').select('.btn-outline-primary').on('click',function(){
+        minBar = d3.min(dataset,function(d){ return +d.clinic});
+        maxBar = d3.max(dataset,function(d){ return +d.clinic})
+        scaleBar = d3.scale.linear()
+        .range([20, 450])
+        .domain([minBar, maxBar]); 
+        
+        rect.selectAll('rect')
+        .data(dataset)
+        .transition()
+        .duration(500)
+        .attr('y',function(d,i){
+            return 500;
+        })
+        .attr('x',function(d,i){
+            return i*40;
+        })
+        .attr('width',30)
+        .attr('height',5)
+        .transition()
+        .duration(1500)
+        .attr('y',function(d,i){
+            return 500-scaleBar(d.clinic);
+        })
+        .attr('height',function(d){
+            return scaleBar(d.clinic);
+
+        })
+        .attr('fill','#12BFF4')
+
+        rect.selectAll('text')
+        .data(dataset)
+        .text(function(d){ return d.clinic})
+        .transition()
+        .duration(500)
+        .attr('y',500-10)
+        .attr('x',function(d,i){
+            return i*40;
+        })
+        .text('0')
+        .tween("text", function(d,i) {
+            var that = d3.select(this),
+                k = d3.interpolateRound(data[i], 0);
+            return function(t) { that.text(k(t)); };
+          })
+        .transition()
+        .duration(1500)
+        .attr('y',function(d,i){
+            return 500-scaleBar(d.clinic)-10;
+        })
+        .tween("text", function(d,i) {
+            var that = d3.select(this),
+                k = d3.interpolateRound(0, +d.clinic);
+                data[i]=+d.clinic;
+            return function(t) { that.text(k(t)); };
+          })
+    });
+
+    d3.select('.block3').select('.btn-outline-danger').on('click',function(){
+        minBar = d3.min(dataset,function(d){ return +d.center});
+        maxBar = d3.max(dataset,function(d){ return +d.center})
+        scaleBar = d3.scale.linear()
+        .range([5, 450])
+        .domain([minBar, maxBar]); 
+        
+        rect.selectAll('rect')
+        .data(dataset)
+        .transition()
+        .duration(500)
+        .attr('y',function(d,i){
+            return 500;
+        })
+        .attr('x',function(d,i){
+            return i*40;
+        })
+        .attr('width',30)
+        .attr('height',5)
+        .transition()
+        .duration(1500)
+        .attr('y',function(d,i){
+            return 500-scaleBar(d.center);
+        })
+        .attr('height',function(d){
+            return scaleBar(d.center);
+
+        })
+        .attr('fill','#F58787')
+
+        rect.selectAll('text')
+        .data(dataset)
+        .text(function(d){ return d.center})
+        .transition()
+        .duration(500)
+        .attr('y',500-10)
+        .attr('x',function(d,i){
+            return i*40+10;
+        })
+        .text('0')
+        .tween("text", function(d,i) {
+            var that = d3.select(this),
+                k = d3.interpolateRound(data[i], 0);
+            return function(t) { that.text(k(t)); };
+          })
+        .transition()
+        .duration(1500)
+        .attr('y',function(d,i){
+            return 500-scaleBar(d.center)-10;
+        })
+        .tween("text", function(d,i) {
+            var that = d3.select(this),
+                k = d3.interpolateRound(0, +d.center);
+                data[i]=+d.center;
+            return function(t) { that.text(k(t)); };
           })
     });
 })
